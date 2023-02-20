48db50aaa9521d8f308f2a01370683d5f01d2546
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2023-02-20 17:31:12 +0000

    removed: unused state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import BottomNav from "./components/BottomNav";
 import Foods from "./components/Foods";
 import Nav from "./components/Nav";
 import "./output.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<div className="min-h-screen bg-[#ededed]">
 			<Nav />
